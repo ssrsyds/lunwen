c795e9a725a3c84d56dad039f3482f6c64fa2610
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-12-07 14:17:46 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,5 +9,24 @@ export default function Header() {
   const { locale, locales, route } = useRouter()
   const otherLocale = locales?.find((cur) => cur !== locale)
 
-  
+  return (
+    <header>
+      
+      <div className="flex gap-2">
+      {
+        otherLocale && (
+          <div
+            className="relative font-medium text-black-600 before:absolute before:-bottom-1 before:h-0.5 before:w-full before:scale-x-0 before:bg-indigo-600 before:transition hover:before:scale-x-100">
+          <Link href={route} locale={otherLocale}>
+            {t('switchLocale', { locale: otherLocale })}
+          </Link>
+          </div>
+        )
+      }
+
+      </div>
+
+
+    </header>
+  );
 }
