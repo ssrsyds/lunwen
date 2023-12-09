4f2ae5d15a09bc786a451171a714d083606de74f
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-12-09 14:13:07 +0800

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -16,7 +16,7 @@ const handler = async (req: Request): Promise<Response> => {
     api_key?: string
   };
   //todo make this variable into messages
-  var p = "我是一个训练有素的大型语言模型，名字叫做智言，可以帮助你回答各种问题，比如关于政治、历史、科学、技术、艺术等方面的问题。我可以帮助你理解一些概念，并为你提供有价值的信息和见解。你可以尝试提出一些问题，看看我能不能帮到你。除了回答问题，我还可以帮助你写一些文章或小说。你只需要给我一些提示和背景信息，我就可以根据你提供的信息来为你写一篇文章或小说。我会尽力为你写出一篇优秀的文章，帮助你实现你的写作目标。用markdown格式以分点叙述的形式输出:"
+  var p = "我是一个训练有素的大型语言模型，充当您的答题助手，可以帮助你回答各种问题。我的性格活泼，擅用语气词，每次回复都会回复1-5个emoji。回答问题时上面是答案，下面是解析。用markdown格式输出:"
   const input = prompt;
   prompt = p + prompt
   if (!prompt) {
@@ -33,10 +33,10 @@ const handler = async (req: Request): Promise<Response> => {
       role: "user",
       content: prompt
     }],
-    temperature: 0.8,
+    temperature: 0.1,
     top_p: 1,
     frequency_penalty: 0,
-    presence_penalty: 2,
+    presence_penalty: 0,
     max_tokens: 10000,
     stream: true,
     n: 1,
